5d500a07221a0ac9cbd188a16d2ebcaa59e61138
Masamune <rodrigo.guerrerovargas@gmail.com>
2018-11-22 22:51:11 -0300

    Initial commit

---
+++ Index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    print "Bienvenidos al curso de PHP <br>"; //imprime y hace un salto de linea (<br>)
+    print "Hola alumnos <br>";
+    print "Hasta el próximo video";
+    /*print "Linea de prueba";*/ // prueba de linea de prueba
+?>
+
+</body>
+</html>
+
+
---
+++ segunda_pagina.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+    $nombre="Rodrigo";
+    $edad=35;
+
+    Print "El nombre del usuario es " . $nombre;
+    echo " y su edad es " . $edad . " años";
+    /*echo $nombre,$edad; */ //echo permite mostrar en pantalla varias variales seguidas //
+
+?>
+
+
+
+
+</body>
+</html>
+
