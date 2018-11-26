0658a4954f46f5d6e7d1e1bf15e48cc56a9cd2d4
Masamune <rodrigo.guerrerovargas@gmail.com>
2018-11-26 12:32:09 -0300

    uploading new files

---
+++ trabajo_strings.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <style>
+
+        .resaltar {
+            color:#FF0000;
+            font-weight: bold;
+
+        }
+    </style>
+
+
+</head>
+
+<body>
+
+<?php
+
+    echo "<p class='resaltar'>Esto es un ejemplo de frase</p>";
+    //tambien se puede utilizar \ antes de las comillas dobles en el caso de no querer utilizar las comillas simples//
+
+?>
+
+</body>
+</html>
+
